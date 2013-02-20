359b24efef9a8a8bbadaed3b4b9861da113403c9
Mathieu Lecarme <mlecarme@bearstech.com>
2013-02-21 00:16:17 +0100

    No more Hello world.

--- app.js
+++ app.js
@@ -2,19 +2,11 @@ var express = require('express'),
     edit = require('./lib/index.js'),
     store = require('bokkusu').jsonstore;
 
-
 var app = express();
 
 //app.use(express.directory('public'));
 app.use(express.static('public'));
 app.use(express.bodyParser());
-
-app.get('/hello.txt', function(req, res) {
-  var body = 'Hello World';
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', body.length);
-  res.end(body);
-});
 
 var bokkusu = store('./demo/sample.json', function(error) {
     edit.crud(app, this);
